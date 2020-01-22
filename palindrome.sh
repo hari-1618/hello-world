4b7b27626bc8465425b7fec413d5645705248572
hari-1618 <54505920+hari-1618@users.noreply.github.com>
2020-01-22 13:33:22 +0530

    Create palindrome.sh

---
+++ palindrome.sh
@@ -0,0 +1,19 @@
+clear
+echo enter a string
+read a
+b='expr $a WC -
+be expr $b - 1
+echo number of letter-Sb
+while test Sb -gt e
+do
+e='expr Sa cut -c $b
+d-$d$e
+be expr $b - 1
+done
+echo the reversed string is :$d
+if test $a = $d
+then
+echo it is a palindrome
+else
+echo it is not a palindrome
+fi
