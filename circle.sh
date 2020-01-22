e88765539ca152af916d6260b962b716cdc70d19
hari-1618 <54505920+hari-1618@users.noreply.github.com>
2020-01-22 13:43:40 +0530

    Neq

--- circle.sh
+++ circle.sh
@@ -1,7 +1,7 @@
-echo "enter radius of circles
+echo "enter radius of circles"
 read r
-area=$(echo "scale;3.14*($r$ | bc)
-d=$(echo "scale-2; 2*$r" bc)
-circumference=$(echo "scale-2.3.14 $d bc)
+area=$(echo "scale=2;3.14*($r*$r)" | bc)
+d=$(echo "scale=2; 2*$r" | bc)
+circumference=$(echo "scale=2;3.14*$d" | bc)
 echo "area of circle is $area"
-echo "circumference of circle is circumference
+echo "circumference of circle is $circumference
