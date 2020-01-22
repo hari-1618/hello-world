5a02d681de4bf10df2f5c4db65176fe68cbd8ee0
hari-1618 <54505920+hari-1618@users.noreply.github.com>
2020-01-22 13:36:05 +0530

    Create prime.sh

---
+++ prime.sh
@@ -0,0 +1,20 @@
+echo enter a limit
+read Limit
+n-2
+while [ $n -le $limit ]
+do
+1-2
+while [ $i -lt Sn ]
+do
+if [ "expr $n X $i -eq
+then
+f-1
+break;
+fi
+1'expr si +1
+done
+If [ A -eq B)
+then
+echo $n
+n expr $n + 1"
+done
