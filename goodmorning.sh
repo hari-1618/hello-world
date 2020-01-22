fde025b99db9bb3b18125d133b8241ed011929d2
hari-1618 <54505920+hari-1618@users.noreply.github.com>
2020-01-22 13:34:45 +0530

    Create goodmorning.sh

---
+++ goodmorning.sh
@@ -0,0 +1,16 @@
+clear
+hours-date cut-c12-13
+if [ Shours le 12 ]
+then
+echo "good morning
+else
+if [ Shours - le 16 )
+then
+echo "good afternoon"
+elif [ Shours -le 20 ]
+then
+echo "good evening"
+else
+echo "good night"
+fi
+fi
